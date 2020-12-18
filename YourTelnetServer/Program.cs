8f165bb717a26d6433c5389c8f8bd777dde605bf
Ihor Boben <53753614+ihorboben@users.noreply.github.com>
2020-12-18 02:15:59 +0200

    Update Program.cs

--- YourTelnetServer/Program.cs
+++ YourTelnetServer/Program.cs
@@ -5,17 +5,30 @@ namespace YourTelnetServer
 {
     class Program
     {
-        private static Server server;
+         private static Server server;
         static void Main(string[] args)
         {
-            server = new Server(IPAddress.Any, 23);
-            server.Start();
-            char read = Console.ReadKey(true).KeyChar;
-
-            do
+            if (args.Length == 0)
             {
+                Console.WriteLine("specify port number");
+                return ;
+            }
 
-            } while ((read = Console.ReadKey(true).KeyChar) != 'q');
+            if (int.TryParse(args[0], out int port))
+            {
+                server = new Server(IPAddress.Any, port);
+                server.Start();
+                char read = Console.ReadKey(true).KeyChar;
+
+                do
+                {
+
+                } while ((read = Console.ReadKey(true).KeyChar) != 'q');
+            }
+            else
+            {
+                Console.WriteLine("specify port number");
+            }
         }
     }
 }
